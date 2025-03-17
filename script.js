f2bb0a4136a3c470b53670d994a3f4061556c541
Felipe Rieger <liperieger@gmail.com>
2025-03-17 12:19:05 -0300

    Merge

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const entre = document.getElementById('entre');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 10) {
         header.style.marginTop = '-30px';
-        logo.style.width = '50px';
+        logo.style.width = '45px';
         logo.style.marginTop = '35px';
         cadastro.style.marginTop = '-10px';
         entre.style.marginTop = '-5px';
