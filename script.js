4cdb87d9d43acf1e81cb16330937d88980881c9a
Felipe Rieger <liperieger@gmail.com>
2025-05-27 10:11:25 -0300

    Ajustes na animação do header

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const header = document.getElementById('header');
 const logo = document.getElementById('logo');
-const cadastro = document.getElementById('cadastro');
+const cadastro = document.getElementById('cadastro_header');
 const entre = document.getElementById('entre');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 10) {
+    if (window.scrollY < 10) {
+        header.style.marginTop = '0px';
+        logo.style.width = '60px';
+        logo.style.marginTop = '11px';
+        logo.style.marginBottom = '7px';
+        cadastro.style.marginTop = '10px';
+        entre.style.marginTop = '17px';
+
+    } else {
         header.style.marginTop = '-30px';
         logo.style.width = '45px';
         logo.style.marginTop = '35px';
         cadastro.style.marginTop = '-10px';
         entre.style.marginTop = '-5px';
-
-    } else {
-        header.style.marginTop = '0px';
-        logo.style.width = '60px';
-        logo.style.marginTop = '11px';
-        logo.style.marginBottom = '7px';
-        cadastro.style.marginTop = '12px';
-        entre.style.marginTop = '18px';
     }
 });
