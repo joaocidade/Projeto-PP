13dba444c63e12ed07e7727b6d458b0da13d43f8
Felipe Rieger <liperieger@gmail.com>
2025-05-28 11:41:47 -0300

    Correção do commit

--- Hub/hubProf.php
+++ Hub/hubProf.php
@@ -1,25 +1,66 @@
 <?php
 include_once '../incluir/conexao.php';
 
+//comando de SQL para executar
 $sql = "SELECT * FROM alunos";
 
+//executa e retorna os dados
 $resultado = mysqli_query($conn,$sql);
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hub do Professor</title>
-    <link rel="stylesheet" href="hubStyle.css">
+    <link rel="stylesheet" href="styleHub.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
+
 <body>
+    <nav class="menulateral">
+        <ul>
+            <li class="itemmenu">
+                <a href="#">
+                    <span class="icon"><i class="bi bi-house"></i></span>
+                    <span class="txt">Inicio</span>
+                </a>
+            </li>
+            <li class="itemmenu">
+                <a href="#">
+                    <span class="icon"><i class="bi bi-camera-reels"></i></span>
+                    <span class="txt">Videos</span>
+                </a>
+            </li> 
+            <li class="itemmenu">
+                <a href="#">
+                    <span class="icon"><i class="bi bi-mic"></i></span>
+                    <span class="txt">Audio</span>
+                </a>
+            </li>
+            <li class="itemmenu">
+                <a href="#">
+                    <span class="icon"><i class="bi bi-hand-index"></i></i></span>
+                    <span class="txt">Libras</span>
+                </a>
+            </li>
+            <li class="itemmenu">
+                <a href="#">
+                    <span class="icon"><i class="bi bi-badge-vr"></i></span>
+                    <span class="txt">VR</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+
     <header>
         <h1> Olá! Seja bem-vindo(a) ao Hub, professor!</h1>
         <div class="txt_recursos">
             <p>Estes são os seus recursos:</p>
         </div>
     </header>
+
     <main>
     <section id="videos">
         <p class='txt'>↳ Vídeos</p>
@@ -122,7 +163,7 @@ $resultado = mysqli_query($conn,$sql);
             Copyright © 2024-2025, development by RA: educação e tecnologia.
         </p>
         
-        </footer>    
-        </div>
-</body>
-</html>
+        </footer>     
+    </body>
+    <script src="menu.js"></script>
+    </html>
