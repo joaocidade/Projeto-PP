20529b890177c45fe826284d1f5e63ed40698055
Felipe Rieger <liperieger@gmail.com>
2025-06-02 11:48:32 -0300

    Ajustes

--- Hub/hubProf.php
+++ Hub/hubProf.php
@@ -14,7 +14,7 @@ $resultado = mysqli_query($conn,$sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hub do Professor</title>
-    <link rel="stylesheet" href="styleHub.css">
+    <link rel="stylesheet" href="styleHub1.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
 
@@ -63,107 +63,70 @@ $resultado = mysqli_query($conn,$sql);
 
     <main>
     <section id="videos">
-        <p class='txt'>↳ Vídeos</p>
-        <div class="vds">
+        <p class='txt_sections'>↳ Vídeos</p>
+        <div class="caixas">
             <div class="caixa">
                 <a href="" target="_blank"><img src="" alt="0"></a>
             </div>
-        <div class="caixa">
-    <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
+            <div class="caixa">
+                <a href="" target="_blank"><img src="" alt="0"></a>
             </div>
             <div class="caixa">
-                <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
+                <a href="" target="_blank"><img src="" alt="0"></a>
             </div>
         </div>
     </section>
     <section id="audio">
-        <div class="txt">
-            <p>
-                ↳ Áudios
-            </p>
+        <p class="txt_sections">↳ Áudios</p>
+        <div class="caixas">
+            <div class="caixa">
+                <a href="" target="_blank"><img src="" alt="0"></a>
+            </div>
+            <div class="caixa">
+                <a href="" target="_blank"><img src="" alt="0"></a>
+            </div>
+            <div class="caixa">
+                <a href="" target="_blank"><img src="" alt="0"></a>
+            </div>
         </div>
-    <div class="aud">
-        <div class="caixa4">
-    <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
-        </div>
-    <div class="caixa5">
-    <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
-        </div>
-        <div class="caixa6">
-    <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
-        </div>
-    </div>
-    </section>
-        <section id="libras">
-        <div class="txt3">
-            <p>
-                ↳ Líbras
-            </p>
-        </div>
-    <div class="lib">
-        <div class="caixa7">
-    <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
-        </div>
-    <div class="caixa8">
-    <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
-        </div>
-        <div class="caixa9">
-    <a href="" target="_blank">
-            <img src="" alt="0">
-            </a>
-        </div>
-    </div>
     </section>
     <section id="libras">
-        <div class="txt">
-            <p>
-                ↳ Alunos
-            </p>
+        <p class="txt_sections">↳ Líbras</p>
+        <div class="caixas">
+            <div class="caixa">
+                <a href="" target="_blank"><img src="" alt="0"></a>
+            </div>
+            <div class="caixa">
+                <a href="" target="_blank"><img src="" alt="0"></a>
+            </div>
+            <div class="caixa">
+                <a href="" target="_blank"><img src="" alt="0"></a>
+            </div>
         </div>
-    <div class="lib">
-        <?php 
-            while ($dado = mysqli_fetch_assoc($resultado)){
-        ?>
-        <tbody>
-            <ul>
-              <li><?php echo $dado['AlunoID'] ?> | <?php echo $dado['Nome'] ?></li>
-            </tr>
-          </tbody>
-        <?php 
-            }
-        ?>
-    </div>
-    <br>
-<br>
-<br>
-<br>
-<br>
+    </section>
+    <section id="libras">
+        <p class="txt_sections">↳ Alunos</p>
+        <div class="caixas">
+            <?php 
+                while ($dado = mysqli_fetch_assoc($resultado)){
+            ?>
+            <tbody>
+                <ul>
+                    <li><?php echo $dado['AlunoID'] ?> | <?php echo $dado['Nome'] ?></li>
+                    </tr>
+                </ul>
+            </tbody>
+            <?php 
+                }
+            ?>
+        </div>
+    </section>
     </main>
     
     <footer>
-
-        <p>
-            Os videoaulas não são de nossa autoria.
-        </p>
-        <p style="font-size: 25px; margin-top:10px;">
-            Copyright © 2024-2025, development by RA: educação e tecnologia.
-        </p>
-        
-        </footer>     
-    </body>
-    <script src="menu.js"></script>
-    </html>
+        <p>Os videoaulas não são de nossa autoria.</p>
+        <p style="font-size: 25px; margin-top:10px;">Copyright © 2024-2025, development by RA: educação e tecnologia.</p>  
+    </footer>     
+</body>
+<script src="menu.js"></script>
+</html>
