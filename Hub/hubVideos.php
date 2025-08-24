eae2bcb945cbab3d0da530d6082de61ab7cbc189
FraNFraN <134552777+BatataDeCodigos@users.noreply.github.com>
2025-08-23 21:49:16 -0300

    adição de imagens e links

--- Hub/hubVideos.php
+++ Hub/hubVideos.php
@@ -103,49 +103,49 @@ $resultado = mysqli_query($conn,$sql);
         </div>
     </section>
     <section id="videos7">
-        <h1 class='classes'>↳ Vídeos de história</h1>
+        <h1 class='classes'>↳ Vídeos de química</h1>
         <div class="caixas">
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=Xi1aDyVrnDg&ab_channel=AalborgUniversitet" target="_blank"><img src="../Imagens/departmentofchemistryandbioscience.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=z9Jm5zR3iSk&ab_channel=UniversityofHelsinki" target="_blank"><img src="../Imagens/radiochemistry.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=RNbNXUh3PH0&ab_channel=UBCOkanaganARCampusTOUR" target="_blank"><img src="../Imagens/" alt=""></a>
             </div>
         </div>
     </section>
     <section id="videos8">
-        <h1 class='classes'>↳ Vídeos de história</h1>
+        <h1 class='classes'>↳ Vídeos de química</h1>
         <div class="caixas">
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=xsgS3yOBveY&t=2s&ab_channel=UniversityofBath" target="_blank"><img src="../Imagens/tourchemistrylab.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=FzGZnZBs_dg&ab_channel=TheUniversityofTulsa" target="_blank"><img src="../Imagens/organicchemistry.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=27yeAXVtlDA&ab_channel=AmandaSimson" target="_blank"><img src="../Imagens/chemicalengineering.png" alt=""></a>
             </div>
         </div>
     </section>
     <section id="videos9">
-        <h1 class='classes'>↳ Vídeos de história</h1>
+        <h1 class='classes'>↳ Vídeos de artes</h1>
         <div class="caixas">
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=V3_XQH0Dy6I&t=20s&ab_channel=6abcPhiladelphia" target="_blank"><img src="../Imagens/" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=U4BoLPZ4qbI&ab_channel=JamesMadisonUniversity" target="_blank"><img src="../Imagens/jmusschoolofart.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="https://www.youtube.com/watch?v=VB_2m2bwrik&t=15s&ab_channel=BryneStothard" target="_blank"><img src="../Imagens/vrartclass.png" alt=""></a>
             </div>
         </div>
     </section>
     <section id="videos10">
-        <h1 class='classes'>↳ Vídeos de história</h1>
+        <h1 class='classes'>↳ Vídeos de artes</h1>
         <div class="caixas">
             <div class="caixa">
                 <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
