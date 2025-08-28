b6a04bf1e4b5f19352ea30e08c504d371b07a6d9
Felipe Rieger <liperieger@gmail.com>
2025-08-28 15:16:30 -0300

    Conversão de vídeos para 360°

--- Hub/hubVideos.php
+++ Hub/hubVideos.php
@@ -22,65 +22,81 @@ $resultado = mysqli_query($conn,$sql);
           <h1 class='classes'>↳ Vídeos de história</h1>
         <div class="caixas">
             <div class="caixa">
-                <a href="../ProjetosWebXR/index.php" target="_blank"><img id="img1" src="../Imagens/All_Gizah_Pyramids.jpg" alt="0"></a>
+                <a href="../ProjetosWebXR/Projeto1/index.php" target="_blank"><img id="img1" src="../Imagens/All_Gizah_Pyramids.jpg" alt="0"></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/0khdjCUIR1g?si=Wh_W06mwF5FVimOU" target="_blank"><img src="../Imagens/economyegypt.png" alt="0"></a>
+                <a href="../ProjetosWebXR/Projeto2/index.php" target="_blank"><img src="../Imagens/trinchesletter.png" alt="0"></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/_JttvM52Rno?si=3jQInBMRifv5VQB9" target="_blank"><img src="../Imagens/exploringegypt.png" alt="0"></a>
+                <a href="../ProjetosWebXR/Projeto3/index.php" target="_blank"><img src="../Imagens/battleroad.png" alt="0"></a>
             </div>
         </div>
     </section>
     <section id="videos2">
         <div class="caixas">
             <div class="caixa">
-                <a href="https://youtu.be/m7h4XuvLrmg?si=Qpp-jgdLglSJ4Hmk" target="_blank"><img id="img1" src="../Imagens/trinchesletter.png" alt="0"></a>
+                <a href="../ProjetosWebXR/Projeto4/index.php" target="_blank"><img id="img1" src="../Imagens/battlewaterloo.png" alt="0"></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/1TqvPNh18ms?si=-IHD_XkcB5b5tYp6" target="_blank"><img src="../Imagens/battleroad.png" alt="0"></a>
+                <a href="../ProjetosWebXR/Projeto5/index.php" target="_blank"><img src="../Imagens/historyofflight.png" alt="0"></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/Bj1aVW3LuVo?si=EeEY2PA939ZrQW7i" target="_blank"><img src="../Imagens/battlewaterloo.png" alt="0"></a>
+                <a href="" target="_blank"><img src="" alt="0"></a>
             </div>
         </div>
     </section>
     <section id="videos3">
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+        <h1 class='classes'>Ciências</h1>
+>>>>>>> Stashed changes
+=======
+        <h1 class='classes'>Ciências</h1>
+>>>>>>> Stashed changes
         <div class="caixas">
             <div class="caixa">
-                <a href="https://youtu.be/G_5r6HCW_DE?si=INJl0WiSZSCQ5hTa" target="_blank"><img src="../Imagens/texashistory.png" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/asteroid.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/JKm3uzL_A4c?si=XdMOqW4df6oR5rLU" target="_blank"><img src="../Imagens/asteroid.png" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/nationalpark.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/fojmorJISL0?si=n-oRkYlmKTm3C0EF" target="_blank"><img src="../Imagens/historyofflight.png" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/interbody.png" alt=""></a>
             </div>
         </div>
     </section>
     <section id="videos4">
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
         <h1 class='classes'>↳ Vídeos de biologia</h1>
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
         <div class="caixas">
             <div class="caixa">
-                <a href="https://youtu.be/v64KOxKVLVg?si=foPVGI2QYP-RJCpb" target="_blank"><img src="../Imagens/nationalpark.png" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/HumanDigestivesys.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/aogLFedcnTI?si=S2EUr3-kqOXHN_au" target="_blank"><img src="../Imagens/interbody.png" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/wthappinsiyrbody.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/r8-6Z1Efgzg?si=nrXpv-HAm4jgIbPF" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/cellexploringvideo.png" alt=""></a>
             </div>
         </div>
     </section>
     <section id="videos5">
         <div class="caixas">
             <div class="caixa">
-                <a href="https://youtu.be/kw9EJbezlK4?si=gPz01_TdLRdqy76v" target="_blank"><img src="../Imagens/wthappinsiyrbody.png" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/departmentofchemistryandbioscience.png" alt=""></a>
             </div>
             <div class="caixa">
-                <a href="https://youtu.be/rmgf0VDDlH8?si=Ba2frVj7Xo0X7yte" target="_blank"><img src="../Imagens/" alt=""></a>
+                <a href="" target="_blank"><img src="../Imagens/radiochemistry.png" alt=""></a>
             </div>
             <div class="caixa">
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
                 <a href="https://www.youtube.com/watch?v=rmgf0VDDlH8&ab_channel=PlantEnergyBiology" target="_blank"><img src="../Imagens/cellexploringvideo.png" alt=""></a>
             </div>
         </div>
@@ -138,6 +154,12 @@ $resultado = mysqli_query($conn,$sql);
             </div>
             <div class="caixa">
                 <a href="" target="_blank"><img src="../Imagens/" alt=""></a>
+=======
+                <a href="" target="_blank"><img src="../Imagens/chemistrylab.png" alt=""></a>
+>>>>>>> Stashed changes
+=======
+                <a href="" target="_blank"><img src="../Imagens/chemistrylab.png" alt=""></a>
+>>>>>>> Stashed changes
             </div>
         </div>
     </section>
---
+++ ProjetosWebXR/Projeto1/index.php
@@ -0,0 +1,105 @@
+<?php
+include_once '../../Incluir/conexao.php';
+include_once '../../Incluir/protect.php';
+
+//comando de SQL para executar
+$sql = "SELECT * FROM alunos";
+
+//executa e retorna os dados
+$resultado = mysqli_query($conn,$sql);
+
+$atulizador = date('YmdHis').rand(0,99999999999);
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Unity WebGL Player | WebXR1</title>
+    <meta name="description" content="">
+    <link rel="shortcut icon" href="../../Imagens/Logo.png">
+    <link rel="stylesheet" href="TemplateData/style.css?t=<?php echo $atulizador;?>">
+  </head>
+  <body>
+    <div id="unity-container">
+      <div id="unity-canvas-container">
+        <canvas id="unity-canvas" style="width: 100%; height: 100%;"></canvas>
+      </div>
+      <div id="unity-loading-bar">
+        <div id="unity-logo"></div>
+        <div id="unity-progress-bar-empty">
+          <div id="unity-progress-bar-full"></div>
+        </div>
+      </div>
+      <div id="unity-footer">
+        <div id="unity-webgl-logo"></div>
+        <button id="entervr" value="Enter VR" disabled>VR</button>
+        <button id="enterar" value="Enter AR" disabled>AR</button>
+      </div>
+    </div>
+    <script>
+      var buildUrl = "Build";
+      var loaderUrl = buildUrl + "/Projeto1.loader.js";
+      var config = {
+        dataUrl: buildUrl + "/Projeto1.data",
+        frameworkUrl: buildUrl + "/Projeto1.framework.js",
+        codeUrl: buildUrl + "/Projeto1.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "WebXR1",
+        productVersion: "0.1",
+      };
+
+      var container = document.querySelector("#unity-container");
+      var canvas = document.querySelector("#unity-canvas");
+      var canvasContainer = document.querySelector("#unity-canvas-container");
+      var loadingBar = document.querySelector("#unity-loading-bar");
+      var progressBarFull = document.querySelector("#unity-progress-bar-full");
+      var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+      var unityInstance = null;
+      
+
+      canvasContainer.style.width = "700px";
+      canvasContainer.style.height = "400px";
+      loadingBar.style.display = "block";
+
+      var script = document.createElement("script");
+      script.src = loaderUrl;
+      script.onload = () => {
+        createUnityInstance(canvas, config, (progress) => {
+          progressBarFull.style.width = 100 * progress + "%";
+        }).then((instance) => {
+          unityInstance = instance;
+          loadingBar.style.display = "none";
+          if (fullscreenButton)
+          {
+            fullscreenButton.onclick = () => {
+              unityInstance.SetFullscreen(1);
+            };
+          }
+        }).catch((message) => {
+          alert(message);
+        });
+      };
+      document.body.appendChild(script);
+
+      let enterARButton = document.getElementById('enterar');
+      let enterVRButton = document.getElementById('entervr');
+
+      document.addEventListener('onARSupportedCheck', function (event) {
+        enterARButton.disabled = !event.detail.supported;
+      }, false);
+      document.addEventListener('onVRSupportedCheck', function (event) {
+        enterVRButton.disabled = !event.detail.supported;
+      }, false);
+
+      enterARButton.addEventListener('click', function (event) {
+        unityInstance.Module.WebXR.toggleAR();
+      }, false);
+      enterVRButton.addEventListener('click', function (event) {
+        unityInstance.Module.WebXR.toggleVR();
+      }, false);
+    </script>
+  </body>
+</html>
